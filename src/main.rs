6a7f67bf344e55cb3a4b8b99e3ed975f350f6f82
Jay/Fienna Liang <roackb2@gmail.com>
2022-11-23 23:27:03 +0800

    Change to functional component and add Stylish

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,26 @@
-use yew::prelude::*;
+use yew::{html, use_state, Callback};
+use stylist::{css, style, StyleSource, YieldStyle};
+use stylist::yew::{styled_component, Global};
 
-enum Msg {
-    AddOne,
-}
+#[styled_component(HelloWorld)]
+fn hello_world() -> Html {
+    let counter = use_state(|| 0);
 
-struct Model {
-    value: i64,
-}
+    let onclick = {
+        let counter = counter.clone();
+        Callback::from(move |_| counter.set(*counter + 1))
+    };
 
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
+    let style = css!(r#"color: red;"#);
 
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            value: 0,
-        }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
-        html! {
-            <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
-        }
-    }
+    html! {
+        <div class={style}>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+     }
 }
 
 fn main() {
-    yew::start_app::<Model>();
+    yew::start_app::<HelloWorld>();
 }
